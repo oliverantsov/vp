ca21e8a250e2c9d36d5c11e65833d7c8275cf9a9
Oliver Antsov <oliverantsov@users.noreply.github.com>
2020-09-20 17:07:02 +0300

    kolmanda tunni materjal

---
+++ tund_3/ideapage.php
@@ -0,0 +1,7 @@
+<a href="home.php"> Tagasi kodulehele </a>
+<hr>
+  <form method="POST"> 
+	<label>Kirjutage oma esimene pähe tulev mõte!</label>
+	<input type="text" name="ideainput" placeholder="mõttekoht">
+	<input type="submit" name="ideasubmit" value="Saada mõte teele!">
+  </form>
