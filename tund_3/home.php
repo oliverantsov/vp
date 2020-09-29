e6cc873ef04879c331a20a04b62d6c721677fb84
Oliver Antsov <oliverantsov@users.noreply.github.com>
2020-09-29 22:22:14 +0300

    uuendatud failid

--- tund_3/home.php
+++ tund_3/home.php
@@ -1,33 +1,5 @@
 <?php
   //var_dump($_POST);
-  require("../../../config.php");
-  $database = "if20_oliver_ant_1";
-  if(isset($_POST["ideasubmit"]) and !empty($_POST["ideainput"])){
-	  ##loome andmebaasiga ühenduse	
-	  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
-	  ##valmistan ette SQL käsu andmete kirjutamiseks
-	  $stmt = $conn->prepare("INSERT INTO myideas (idea) VALUES(?)");
-	  echo $conn->error;
-	  //i - integer, d - decimal, s - string
-	  $stmt->bind_param("s", $_POST["ideainput"]);
-	  $stmt->execute();
-	  $stmt->close();
-	  $conn->close();
-  }
-  
-  //loen andmebaasist senised mõtted
-  $ideahtml = "";
-  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
-  $stmt = $conn->prepare("SELECT idea FROM myideas");
-  //seon tulemuse muutujaga
-  $stmt->bind_result($ideafromdb);
-  $stmt->execute();
-  while ($stmt->fetch()){
-	  $ideahtml .= "<p>" .$ideafromdb ."</p>";
-  }
-  $stmt->close();
-  $conn->close();
-  
   $username = "Oliver Antsov";
   $datenow = date("d.");
   $yearnow = date("Y");
@@ -39,7 +11,7 @@
   $monthnameset = ["jaanuar", "veebruar", "märts", "aprill", "mai", "juuni", "juuli", "august", "september", "oktoober", "november", "detsember"];
   ##echo $weekdaynameset[1];
   $weekdaynow = date("N");
-  $monthnow = date("N");
+  $monthnow = date("n");
   
   if ($hournow > 23 and $hournow <= 7);{
 	$partofday = "uneaeg";
@@ -69,12 +41,12 @@
   $semestercurrent = $semesterstart->diff($today);
   $semestercurrentdays = $semestercurrent->format("%r%a");
   if ($semestercurrentdays < 0);{
-	  $semestercurrent = "Antud semester pole veel alanud!";
+	  $semestercurrentdays = "Antud semester pole veel alanud!";
 	}
-  if ($semestercurrentdays > $semesterend);{
-	  $semesterremaining = "Antud semester on juba lõppenud!";
+  if ($semestercurrentdays > $semesterdurationdays);{
+	  $semestercurrentdays = "Antud semester on juba lõppenud!";
 	}
-	
+
 ## if($fromsemesterstartdays < 0) (semester pole peale hakanud) 
 ## leiame erinevuse tänasega (semesterduration jne)
   
@@ -90,24 +62,26 @@
   ##$i = $i + 1;
   ##$i ++;
   //$i += 3
-  for($i = 0;$i < $randpic; $i ++){
+  ##for($i = 0;$i < $randpic; $i ++){
 	  ##<img src="../img/pildifail" alt="tekst">
-	  $imghtml .= '<img src="../img/' .$picfiles[$i] .'" alt="Tallinna Ülikool">';
-	}
+	  ##$imghtml .= '<img src="../img/' .$picfiles[$i] .'" alt="Tallinna Ülikool">';
+	##}
+  $imghtml .= '<img src="../img/' .$picfiles[$randpic] .'" alt="Tallinna Ülikool">';
   require("header.php");
 ?>
 <body>
   <img src="../vp_pics/vp_banner.png" alt="Veebiprogrammeerimise kursuse logo">
   <h1><?php echo $username; ?></h1>
   <a href="ideapage.php"> Mõtete sisestamise leht </a> <br>
-  <a href="ideaanswers.php"> Mõtete vastuste leht </a>
+  <a href="ideaanswers.php"> Mõtete vastuste leht </a> <br>
+  <a href="listfilms.php"> Filmide nimekirja leht </a> <br>
   <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
   <p>Leht on loodud veebiprogrammeerimise kursuse raames <a href="https://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
   <h1>Minust</h1>
   <p>Olen 20-aastane noormees Raplamaalt ning õpin Tallinna Ülikoolis informaatika erialal.</p>
-  <p>Lehe avamise hetkel oli: <?php echo $weekdaynameset[$weekdaynow - 1] .", " .$datenow ." " .$monthnameset[$monthnow +1] ." " .$yearnow .", kell " .$clocknow; ?>.</p>
+  <p>Lehe avamise hetkel oli: <?php echo $weekdaynameset[$weekdaynow - 1] .", " .$datenow ." " .$monthnameset[$monthnow -1] ." " .$yearnow .", kell " .$clocknow; ?>.</p>
   <p><?php echo "Parajasti on " .$partofday ."."; ?></p> 
-  <p><?php echo "Praeguse semestri kestvus algusest: " .$semestercurrentdays ." päeva"; ?></p>
+  <p><?php echo "Semestri kestvus päevades: " .$semestercurrentdays; ?></p>
   <hr>
   <?php echo $imghtml; ?>
   
--- tund_3/ideapage.php
+++ tund_3/ideapage.php
@@ -1,3 +1,34 @@
+<?php
+//var_dump($_POST);
+  require("../../../config.php");
+  $database = "if20_oliver_ant_1";
+  if(isset($_POST["ideasubmit"]) and !empty($_POST["ideainput"])){
+	  ##loome andmebaasiga ühenduse	
+	  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+	  ##valmistan ette SQL käsu andmete kirjutamiseks
+	  $stmt = $conn->prepare("INSERT INTO myideas (idea) VALUES(?)");
+	  echo $conn->error;
+	  //i - integer, d - decimal, s - string
+	  $stmt->bind_param("s", $_POST["ideainput"]);
+	  $stmt->execute();
+	  $stmt->close();
+	  $conn->close();
+  }
+  
+  //loen andmebaasist senised mõtted
+  $ideahtml = "";
+  $conn = new mysqli($serverhost, $serverusername, $serverpassword, $database);
+  $stmt = $conn->prepare("SELECT idea FROM myideas");
+  //seon tulemuse muutujaga
+  $stmt->bind_result($ideafromdb);
+  $stmt->execute();
+  while ($stmt->fetch()){
+	  $ideahtml .= "<p>" .$ideafromdb ."</p>";
+  }
+  $stmt->close();
+  $conn->close();
+?>
+
 <a href="home.php"> Tagasi kodulehele </a>
 <hr>
   <form method="POST"> 
